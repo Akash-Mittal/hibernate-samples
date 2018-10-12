7e21a0843b07906d9647eaae4bd0086b6d5e86ce
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-10-13 00:51:24 +0530

    Changes:1.) Added Simple Test Cases.

--- hibernate-samples-composite-key/src/main/java/com/am/innovations/hibernate/jpa/Application.java
+++ hibernate-samples-composite-key/src/main/java/com/am/innovations/hibernate/jpa/Application.java
@@ -1,32 +1,15 @@
 package com.am.innovations.hibernate.jpa;
 
-import java.math.BigDecimal;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class Application implements CommandLineRunner {
+public class Application {
 
-	@Autowired
-	private WalletRepository walletRepository;
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		WalletPK walletPK = new WalletPK(123L, CURRENCY.EUR);
-		Wallet wallet = new Wallet(walletPK, BigDecimal.TEN);
-		walletRepository.save(wallet);
-
-		System.out.println(
-				walletRepository.findById(new WalletPK.Builder().currency(CURRENCY.EUR).userID(123L).build()).get());
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
--- hibernate-samples-one-to-many/src/test/java/com/am/innovations/hibernate/jpa/ApplicationTests.java
+++ hibernate-samples-one-to-many/src/test/java/com/am/innovations/hibernate/jpa/ApplicationTests.java
@@ -3,37 +3,21 @@ package com.am.innovations.hibernate.jpa;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("test")
 public class ApplicationTests {
 
-	@Autowired
-	private HouseRepository houseRepository;
+    @Before
+    public void setUp() throws Exception {
+    }
 
-	@Autowired
-
-	private OwnerRepository ownerRepository;
-
-	@Before
-	public void setUp() throws Exception {
-		ownerRepository.deleteAll();
-		houseRepository.deleteAll();
-
-	}
-
-	@Test
-	public void contextLoads() {
-		for (int i = 0; i < 10; i++) {
-			House house = new House("House Villa #" + i);
-			Owner owner = new Owner("Owner#" + i);
-			owner.getHouse().add(house);
-			house.setOwner(owner);
-			ownerRepository.save(owner);
-		}
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
