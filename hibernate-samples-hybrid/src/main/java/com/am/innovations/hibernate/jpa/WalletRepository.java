7a6954a4f20918aefa126073bfb1aaa723973212
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-10-19 13:53:49 +0530

    Changes:1.) Fixed https://github.com/Akash-Mittal/hibernate-samples/issues/3

--- hibernate-samples-hybrid/src/main/java/com/am/innovations/hibernate/jpa/WalletRepository.java
+++
@@ -1,7 +0,0 @@
-package com.am.innovations.hibernate.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WalletRepository extends JpaRepository<Wallet, Long> {
-
-}
--- hibernate-samples-hybrid/src/test/java/com/am/innovations/hibernate/jpa/UserRepositoryTest.java
+++ hibernate-samples-hybrid/src/test/java/com/am/innovations/hibernate/jpa/UserRepositoryTest.java
@@ -1,39 +1,60 @@
 package com.am.innovations.hibernate.jpa;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertNotNull;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-//@ActiveProfiles("test")
+@ActiveProfiles("test")
 public class UserRepositoryTest {
 
-	@Autowired
-	private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-	@Autowired
-	private WalletRepository walletRepository;
+    @Autowired
+    private BalanceRepository balanceRepository;
 
-	@Before
-	public void setUp() throws Exception {
-		userRepository.deleteAll();
-	}
+    @Before
+    public void setUp() throws Exception {
+        userRepository.deleteAll();
+    }
 
-	@Test
-	public void testSaveAndFind() throws Exception {
-		User user1 = new User("Adam");
-		User user2 = new User("Mike");
-		User user3 = new User("John");
+    @Test
+    public void testSaveAndFind() throws Exception {
+        User user1 = new User("Adam");
+        User user2 = new User("Mike");
+        User user3 = new User("John");
 
-		assertNotNull(userRepository.save(user1));
-		assertThat(userRepository.findAll().size()).isEqualTo(1);
+        Balance balance = new Balance(CURRENCY.EUR, BigDecimal.TEN);
+        user1.getBalance().add(balance);
+        balance.setUser(user1);
 
-	}
+        assertNotNull(userRepository.save(user1));
+        assertNotNull(balanceRepository.save(balance));
+
+        assertNotNull(userRepository.save(user2));
+        assertNotNull(userRepository.save(user3));
+
+        List<User> userList = userRepository.findAll();
+        assertNotNull(userList);
+        Assert.assertThat(userList.size(), is(3));
+        assertNotNull(userList.get(0).getBalance());
+        assertNotNull(userList.get(0).getBalance().get(0));
+
+        Assert.assertThat(userList.get(0).getBalance().get(0).getCurrency(), is(CURRENCY.EUR));
+        Assert.assertTrue(userList.get(0).getBalance().get(0).getBalance().compareTo(BigDecimal.TEN) == 0);
+
+    }
 }
