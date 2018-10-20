cf5529848dd1d85b0a583e1b6b91c21cd40c724c
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-10-21 00:17:47 +0530

    Changes:1.) Intermediate.

---
+++ hibernate-samples-hybrid/src/main/java/com/am/innovations/hibernate/jpa/BalanceRepository.java
@@ -0,0 +1,7 @@
+package com.am.innovations.hibernate.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BalanceRepository extends JpaRepository<Balance, Long> {
+
+}
