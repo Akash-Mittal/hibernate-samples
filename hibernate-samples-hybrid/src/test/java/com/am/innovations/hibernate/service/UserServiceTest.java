464c8ac80626bb397cd74d589fe0bbaf5d81625f
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-10-23 14:35:44 +0530

    Changes:1.) Refactored

--- hibernate-samples-hybrid/src/test/java/com/am/innovations/hibernate/service/UserServiceTest.java
+++ hibernate-samples-hybrid/src/test/java/com/am/innovations/hibernate/service/UserServiceTest.java
@@ -2,6 +2,7 @@ package com.am.innovations.hibernate.service;
 
 import java.math.BigDecimal;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,6 @@ import com.am.innovations.hibernate.entities.Balance;
 import com.am.innovations.hibernate.entities.User;
 import com.am.innovations.hibernate.enums.CURRENCY;
 import com.am.innovations.hibernate.repo.UserRepository;
-import com.am.innovations.hibernate.service.UserService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -29,14 +29,10 @@ public class UserServiceTest {
 	@Test
 	public void testDeposit() throws Exception {
 		User user1 = new User(1l, "Adam");
-		User user2 = new User(2l, "Mike");
-		User user3 = new User(3l, "John");
-
 		Balance balance = new Balance(CURRENCY.EUR, BigDecimal.TEN);
 		user1.getBalance().put(CURRENCY.EUR, balance);
 		userRepository.save(user1);
-		userService.deposit(user1.getUserID(), balance.getCurrency(), balance.getBalance());
-
+		Assert.assertTrue(userService.deposit(user1.getUserID(), balance.getCurrency(), balance.getBalance()));
 	}
 
 }
