1f8e2191a9845b3019f659b9a6e84d068ab4c4f2
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-10-23 14:30:25 +0530

    Changes:1.) Refactored

---
+++ hibernate-samples-hybrid/src/main/java/com/am/innovations/hibernate/Application.java
@@ -0,0 +1,32 @@
+package com.am.innovations.hibernate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.am.innovations.hibernate.entities.User;
+import com.am.innovations.hibernate.repo.UserRepository;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class Application implements CommandLineRunner {
+	@Autowired
+	private UserRepository userRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	@Order(value = Ordered.HIGHEST_PRECEDENCE)
+	public void run(String... args) throws Exception {
+		for (int i = 0; i < 10; i++) {
+			userRepository.save(new User("user." + i));
+		}
+	}
+
+}
